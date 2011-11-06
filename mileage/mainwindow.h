9bb85b482314c1f05805fad7f489e7ef3061c46b
acecil <gascoyne@gmail.com>
2011-11-06 17:52:33 +0000

    Fix mainwindow.h comment.

--- mileage/mainwindow.h
+++ mileage/mainwindow.h
@@ -1,22 +1,4 @@
 /*
-    This file is part of "mileage".
-
-    "mileage" is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Foobar is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-#ifndef MAINWINDOW_H
-/*
     This file is part of Mileage.
 
     Mileage is free software: you can redistribute it and/or modify
@@ -33,6 +15,7 @@
     along with Mileage.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
 #include <QtGui/QMainWindow>
